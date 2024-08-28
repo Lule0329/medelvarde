c218963130c53d307597c7e05065fe1330c38d35
Ludwig Lennartsson <ludwig.lennartsson@skola.sala.se>
2024-08-28 13:23:49 +0200

    Add project files.

---
+++ medelvärde/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace medelvärde
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Run();
+        }
+
+        static void Run()
+        {
+            Console.WriteLine("Skriv in ett heltal");
+            double number = int.Parse(Console.ReadLine());
+            Console.Clear();
+            Console.WriteLine("Medelvärdet är: " + Medelvärde(number));
+            Run();
+        }
+        
+        static double Medelvärde(double number)
+        {
+            GlobalVariables.Sum += number;
+            GlobalVariables.NumberAmount++;
+
+            double medelVärde = GlobalVariables.Sum / GlobalVariables.NumberAmount;
+            return medelVärde;
+        }
+
+        public class GlobalVariables
+        {
+            public static double Sum = 0;
+            public static double NumberAmount = 0;
+        }
+    }
+}
